d13a1174b347b990be5534b7a7764d7d014224b0
gregor_p <gpribb@gmail.com>
2014-07-30 18:46:13 +0200

    new test

--- script_geisa.py
+++ script_geisa.py
@@ -3,9 +3,8 @@
 import time
 import csv
 
-
 with open("/root/tanken/output_geisa.txt") as f:
-        content = f.read().replace('\n', '')
+  content = f.read().replace('\n', '')
 
 start = content.find("price:")
 price = content[start+8:start+13].replace('.', ',')
@@ -15,4 +14,3 @@ RESULT = [time,price]
 resultFile = open("/root/tanken/output_geisa.csv",'a')
 wr = csv.writer(resultFile, delimiter=";")
 wr.writerow(RESULT)
-
