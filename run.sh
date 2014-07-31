c79b81f6e0a743d6426fd531e7a12fa85114bd45
gregor <gpribb@gmail.com>
2014-07-31 17:43:45 +0200

    delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl http://tanken.t-online.de/tankstelle/diesel/519b4914bed7139bf0d43042 -o /root/tanken/output_geisa.txt
-curl http://tanken.t-online.de/tankstelle/diesel/519b48babed7139bf0d42513 -o /root/tanken/output.txt
-sleep 2
-python /root/tanken/script.py
-python /root/tanken/script_geisa.py
